7e5e7d7464861d04a12596439b917ff5b7838c3c
Nachtkind <robin.keinert@gmx.net>
2020-11-27 12:07:58 +0100

    Dateien angepaast mit hilfe von ibmcloud Oberfläche.

--- src/detect-language.js
+++ src/detect-language.js
@@ -3,9 +3,9 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 
 
 /**
- * Helper 
- * @param {*} errorMessage 
- * @param {*} defaultLanguage 
+ * Helper
+ * @param {*} errorMessage
+ * @param {*} defaultLanguage
  */
 function getTheErrorResponse(errorMessage, defaultLanguage) {
   return {
@@ -17,15 +17,28 @@ function getTheErrorResponse(errorMessage, defaultLanguage) {
   };
 }
 
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: 'GiTvLRkhth8lAWupeWYGU9xkj00mbOoA1rdskzMG9U9V',
+  }),
+  serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/84e5d643-75f0-44c6-b229-2f93eb640d3c',
+});
+
+const identifyParams = {
+  text: 'Das ist ein test'
+};
+
+
 /**
-  *
-  * main() will be run when the action is invoked
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
+ *
+ * main() will be run when the action is invoked
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
 function main(params) {
 
   /*
@@ -36,7 +49,7 @@ function main(params) {
   return new Promise(function (resolve, reject) {
 
     try {
-      
+
       // *******TODO**********
       // - Call the language identification API of the translation service
       // see: https://cloud.ibm.com/apidocs/language-translator?code=node#identify-language
@@ -44,23 +57,8 @@ function main(params) {
       // language that is most probable the best one in the "language" property
       // and the confidence it got detected in the "confidence" property
 
-      // in case of errors during the call resolve with an error message according to the pattern 
+      // in case of errors during the call resolve with an error message according to the pattern
       // found in the catch clause below
-
-      const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-      const { IamAuthenticator } = require('ibm-watson/auth');
-
-      const languageTranslator = new LanguageTranslatorV3({
-        version: '2018-05-01',
-        authenticator: new IamAuthenticator({
-          apikey: 'GiTvLRkhth8lAWupeWYGU9xkj00mbOoA1rdskzMG9U9V',
-        }),
-        serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/84e5d643-75f0-44c6-b229-2f93eb640d3c',
-      });
-
-      const identifyParams = {
-        text: 'Language translator translates text from one language to another'
-      };
 
       languageTranslator.identify(identifyParams)
           .then(identifiedLanguages => {
@@ -68,9 +66,9 @@ function main(params) {
             resolve({
               statusCode: 200,
               body: {
-                text: params.text,
-                language: identifiedLanguages[0].language,
-                confidence: identifiedLanguages[0].confidence,
+                text: identifyParams.text,
+                language: identifiedLanguages.result.languages[0].language,
+                confidence: identifiedLanguages.result.languages[0].confidence,
               },
               headers: { 'Content-Type': 'application/json' }
             });
--- src/translate.js
+++ src/translate.js
@@ -3,9 +3,9 @@ const { IamAuthenticator } = require('ibm-watson/auth');
 
 
 /**
- * Helper 
- * @param {*} errorMessage 
- * @param {*} defaultLanguage 
+ * Helper
+ * @param {*} errorMessage
+ * @param {*} defaultLanguage
  */
 function getTheErrorResponse(errorMessage, defaultLanguage) {
   return {
@@ -17,21 +17,38 @@ function getTheErrorResponse(errorMessage, defaultLanguage) {
   };
 }
 
+const languageTranslator = new LanguageTranslatorV3({
+  version: '2018-05-01',
+  authenticator: new IamAuthenticator({
+    apikey: 'GiTvLRkhth8lAWupeWYGU9xkj00mbOoA1rdskzMG9U9V',
+  }),
+  serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/84e5d643-75f0-44c6-b229-2f93eb640d3c',
+});
+
+
+
+
 /**
-  *
-  * main() will be run when teh action is invoked
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
+ *
+ * main() will be run when teh action is invoked
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
 function main(params) {
 
   /*
    * The default language to choose in case of an error
    */
   const defaultLanguage = 'en';
+
+  const translateParams = {
+    text: params.test_text.text,
+    modelId: 'de-en',
+  };
+
 
   return new Promise(function (resolve, reject) {
 
@@ -50,31 +67,16 @@ function main(params) {
 
       // pick the language with the highest confidence, and send it back
 
-      const LanguageTranslatorV3 = require('ibm-watson/language-translator/v3');
-      const { IamAuthenticator } = require('ibm-watson/auth');
 
-      const languageTranslator = new LanguageTranslatorV3({
-        version: '2018-05-01',
-        authenticator: new IamAuthenticator({
-          apikey: 'GiTvLRkhth8lAWupeWYGU9xkj00mbOoA1rdskzMG9U9V',
-        }),
-        serviceUrl: 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/84e5d643-75f0-44c6-b229-2f93eb640d3c',
-      });
-
-      const translateParams = {
-        text: 'Hello, how are you today?',
-        modelId: 'en-es',
-      };
-
-      languageTranslator.translate(translateParams)
+      languageTranslator.translate(translateParams) //params.(...)
           .then(translationResult => {
             console.log(JSON.stringify(translationResult, null, 2));
             resolve({
               statusCode: 200,
               body: {
-                translations: translationResult.translations,
-                words: translationResult.word_count,
-                characters:translationResult.character_count,
+                translations: translationResult.result.translations[0].translation,
+                words: translationResult.result.translations[0].word_count,
+                characters:translationResult.result.translations[0].character_count,
               },
               headers: { 'Content-Type': 'application/json' }
             });
@@ -85,10 +87,11 @@ function main(params) {
           });
 
 
-         
+
     } catch (err) {
       console.error('Error while initializing the AI service', err);
       resolve(getTheErrorResponse('Error while communicating with the language service', defaultLanguage));
     }
   });
 }
+
